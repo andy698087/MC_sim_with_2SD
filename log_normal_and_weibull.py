efcbf16f90f1d0303ed4160b852a932f3f447591
andy698087 <andy698087@hotmail.com>
2023-09-22 17:50:37 +0800

    not minimize

--- log_normal_and_weibull.py
+++ log_normal_and_weibull.py
@@ -1,16 +1,45 @@
 import numpy as np
-import scipy.stats as stats
+from scipy.stats import norm, weibull_min
+from math import gamma, log, sqrt
+from scipy.optimize import minimize
+
+def sum_diff_weibull_lognorm(params):
+    shape_parameter, scale_parameter = params
+    N = 25
+    MeanTimeScale = 1
+    CVTimeScale = 0.3
+
+    MeanLogScale = log(MeanTimeScale)
+    SDLogScale = sqrt(log(1 + CVTimeScale ** 2))
+    SamplesLogNorm = np.exp([(norm.ppf(i,loc=MeanLogScale, scale=SDLogScale)) for i in np.random.rand(N)] )
+    SampleMeanLogNorm = np.mean(SamplesLogNorm)
+    SampleVarianceLogNorm = np.var(SamplesLogNorm)
+
+    SamplesWeibull = weibull_min.rvs(shape_parameter, loc=0, scale=scale_parameter, size=N)
+    SampleMeanWeibull = np.mean(SamplesWeibull)
+    SampleVarianceWeibull = np.var(SamplesWeibull)
+    return SampleMeanWeibull-SampleMeanLogNorm+SampleVarianceWeibull-SampleVarianceLogNorm
+
+
+x0 = [0.1, 1]
+bounds = ((0,None),(0,None))
+res = minimize(sum_diff_weibull_lognorm, x0, tol = 1e-6, bounds=bounds)
+print(res.x)
+
+print(sum_diff_weibull_lognorm(res.x))
+
+quit()
 
 # 设置Weibull分布的参数（根据所选参数化方式）
 shape_parameter = 2.0  # 形状参数(c)
 scale_parameter = 3.0  # 尺度参数(λ或scale)
 
 # 计算理论均值和方差
-theory_mean = scale_parameter * np.gamma(1 + 1/shape_parameter)
-theory_variance = (scale_parameter ** 2) * (np.gamma(1 + 2/shape_parameter) - (np.gamma(1 + 1/shape_parameter)) ** 2)
+theory_mean = scale_parameter * gamma(1 + 1/shape_parameter)
+theory_variance = (scale_parameter ** 2) * (gamma(1 + 2/shape_parameter) - (gamma(1 + 1/shape_parameter)) ** 2)
 
 # 生成模拟数据
-num_samples = 1000  # 模拟样本数量
+num_samples = 30  # 模拟样本数量
 simulated_data = stats.weibull_min.rvs(shape_parameter, loc=0, scale=scale_parameter, size=num_samples)
 
 # 计算模拟数据的样本均值和样本方差
