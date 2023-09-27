d5a6e55078b593d970837b99e8cf0fbc2b41172a
andy698087 <andy698087@hotmail.com>
2023-09-27 19:28:22 +0800

    check sim among sim to be identical

--- sim_among_sim.py
+++ sim_among_sim.py
@@ -9,7 +9,12 @@ from scipy.stats import norm
 from datetime import datetime
 
 
-def making_random_pick(seed_unique):
+def making_random_pick(row, nSample):
+    seed(row)
+    df_rand = pd.DataFrame({'index_': range(100000), 'rand': rand(100000)})
+    return df_rand.sort_values(by='rand', ascending=True).iloc[:nSample, 0].tolist()
+
+def making_random_pick_no_replacement(seed_unique):
     seed(seed_unique)
     df_rand = pd.DataFrame({'index_':range(100000),'rand':rand(100000)})
     return df_rand.sort_values(by='rand',ascending=True)['index_'].tolist()
@@ -31,10 +36,12 @@ start_time = datetime.now() # record the datetime at the start
 print('start_time:', start_time) # print the datetime at the start
 
 # Define the directory where your text files are located
-directory = "C:/Users/User/MC_sim_2SD/Weibull_GPM_MC_2SD_20230925"
+directory = "C:/Users/User/MC_sim_2SD/Weibull_GPM_MC_2SD_20230925_two_moments_no_label_no_moments_not_LogSample"
+# directory = "C:/Users/User/MC_sim_2SD/GPM_MC_2SD_higher_orders_2_compare_20230916_LogNorm"
 
 # Get a list of files that match the pattern in the directory
 matching_files = [file for file in os.listdir(directory) if file.startswith("Weibull_GPM_MC_nMonte_100000_N_") and file.endswith(".csv")]
+# matching_files = [file for file in os.listdir(directory) if file.startswith("GPM_MC_nMonte_100000_N_15_CV_0.3_20230916192959_first_two_moment") and file.endswith(".csv")]
 
 # Initialize an empty list to store data from all files
 
@@ -48,6 +55,7 @@ seed_ = 20230908
 
 # Loop through the matching files and extract data
 for filename in matching_files:
+    print(filename)
     filename_frag = filename.rstrip('.csv').split('_')
     # if filename_frag[-1] == 'noMethodOfMoments' or filename_frag[-1] == 'False':
     #     continue
@@ -77,15 +85,19 @@ for filename in matching_files:
         data['N'].append(filename_frag[6])
         data['CV'].append(filename_frag[8])
         data['nSample'].append(nSample)
-        nSim = int(len(df)/nSample) #10000
+        nSim = 10000 #int(len(df)/nSample) #10000
 
+        # with replacement
         # df_for_dd = pd.DataFrame({'seed_':range(seed_, seed_ + nSim)})
-        
-        rand_index = making_random_pick(seed_)
-        sublist = [rand_index[i:i+10] for i in range(0, len(rand_index), 10)]
-        # df_for_dd = df_for_dd['seed_'].apply(making_random_pick, args=(seed_))
+        # df_for_dd = df_for_dd['seed_'].apply(making_random_pick, args=(nSample, ))
+        # ddf = dd.from_pandas(df_for_dd, npartitions=30)
+                
+        # no replacement
+        rand_index = making_random_pick_no_replacement(seed_)
+        sublist = [rand_index[i:i+10] for i in range(0, nSample*nSim, 10)]
         df_for_dd = pd.DataFrame({'rand_index':sublist})
         ddf = dd.from_pandas(df_for_dd['rand_index'], npartitions=30)
+
         ddf= ddf.apply(sample_weighted_ave_se_interval_include, meta=('float64', 'float64'))
         df_record = pd.DataFrame(ddf.compute().tolist(), columns=['weighted_ave', 'weighted_se', 'intervals_include_zero'])
 
@@ -102,7 +114,7 @@ df[df.columns[1:]] = df[df.columns[1:]].astype(float)
 
 end_time = datetime.now() # record the datetime at the end
 print('end_time:', end_time) # print the datetime at the end
-df.to_csv(f"From_text_GPM_MC_nMonte_100000_N_Higgins1MoM_resample_mean_se_{str(end_time).split('.')[0].replace('-','').replace(' ','').replace(':','')}.csv")
+df.to_csv(f"Resample_NR_mean_weighted_ave_ln_ratio_dask_{str(end_time).split('.')[0].replace('-','').replace(' ','').replace(':','')}.csv")
 
 time_difference = end_time - start_time
 print('time_difference:', time_difference) # calculate the time taken
--- sim_among_sim_for_publication.py
+++ sim_among_sim_for_publication.py
@@ -10,6 +10,11 @@ def making_random_pick(seed_i, nSample):
     df_rand = pd.DataFrame({'index_': range(100000), 'rand': rand(100000)})
     return df_rand.sort_values(by='rand', ascending=True).iloc[:nSample, 0].tolist()
 
+def making_random_pick_no_replacement(seed_unique):
+    seed(seed_unique)
+    df_rand = pd.DataFrame({'index_':range(100000),'rand':rand(100000)})
+    return df_rand.sort_values(by='rand',ascending=True)['index_'].tolist()
+
 # Function to calculate the weighted average of a sample
 def sample_weighted_ave_se_interval_include(list_choose_no, df_GPM_MC_ln_ratio_se):
     sum_wi_Ti, sum_wi_ = 0, 0
@@ -25,8 +30,8 @@ start_time = datetime.now()
 print('start_time:', start_time)
 
 # Define the file_path where your csv files are located
-csv_file_path = "/Users/andypchen/Dropbox/MC_sim/GPM_MC_nMonte_100000_N_50_CV_0.15_20230916014632_higher_orders_of_moments_noRawSamples.csv"
-
+# csv_file_path = "/Users/andypchen/Dropbox/MC_sim/GPM_MC_nMonte_100000_N_50_CV_0.15_20230916014632_higher_orders_of_moments_noRawSamples.csv"
+csv_file_path = "GPM_MC_2SD_higher_orders_2_compare_20230916_LogNorm/GPM_MC_nMonte_100000_N_15_CV_0.3_20230916192959_first_two_moment.csv"
 # Initialize an empty dictionary to store data from all files
 data = {'mean_weighted_ave_ln_ratio': [], 'se_weighted_ave_ln_ratio': []}
 
@@ -37,20 +42,27 @@ z_score = norm.ppf(1 - alpha / 2)
 # Number of Samples to be sampled each iteration
 nSamples = 10
 # Number of simulations
-nSim = 100000
+nSim = 100
 
 # Extract relevant information from csv file, including columns of ln_ratio and se_ln_ratio
 df_GPM_MC_ln_ratio_se = pd.read_csv(csv_file_path)
-
+print(df_GPM_MC_ln_ratio_se)
 seed_ = 20230908
 list_seed = [s for s in range(seed_, seed_ + nSim)]
 list_weighted_ave_ln_ratio = []
 
 # Loop through different seeds, calculate weighted averages, and record in list
-for seed_i in list_seed:
-    random_pick_nSamples = making_random_pick(seed_i, nSamples)
-    list_weighted_ave_ln_ratio.append(sample_weighted_ave_se_interval_include(random_pick_nSamples, df_GPM_MC_ln_ratio_se))
+# for seed_i in list_seed:
+    # random_pick_nSamples = making_random_pick(seed_i, nSamples)
+    # list_weighted_ave_ln_ratio.append(sample_weighted_ave_se_interval_include(random_pick_nSamples, df_GPM_MC_ln_ratio_se))
 
+# no replacement
+rand_index = making_random_pick_no_replacement(seed_)
+random_pick_nSamples = [rand_index[i:i+10] for i in range(0, nSamples*nSim, 10)]
+print(random_pick_nSamples)
+for random_pick_nSample in random_pick_nSamples:
+    list_weighted_ave_ln_ratio.append(sample_weighted_ave_se_interval_include(random_pick_nSample, df_GPM_MC_ln_ratio_se))
+print(list_weighted_ave_ln_ratio)
 # Convert the list to a pandas Series for calculating mean and standard deviation
 list_weighted_ave_ln_ratio = pd.Series(list_weighted_ave_ln_ratio)
 data['mean_weighted_ave_ln_ratio'].append(list_weighted_ave_ln_ratio.mean())
@@ -67,7 +79,7 @@ time_difference = end_time - start_time
 print('time_difference:', time_difference)
 
 # Define the output filename for the CSV file with timestamp
-output_dir = f"Resample_mean_weighted_ave_ln_ratio_{str(end_time).split('.')[0].replace('-', '').replace(' ', '').replace(':', '')}.csv"
+output_dir = f"Resample_NR_mean_weighted_ave_ln_ratio_{str(end_time).split('.')[0].replace('-', '').replace(' ', '').replace(':', '')}.csv"
 
 # Save the DataFrame to a CSV file
 df_data.to_csv(output_dir)
